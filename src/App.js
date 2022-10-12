aeb543a5a25bbae9fbbd80ac195a8df118a96247
Julihumere <julihumereuner@gmail.com>
2022-10-12 13:15:35 -0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import logo from "./img/logo.png";
+import stadium from "./img/estadio_lusail.jpg";
+import groups from "./img/grupos.png";
 
 function App() {
+  const [time, setTime] = useState({
+    days: "",
+    hours: "",
+    minutes: "",
+    seconds: "",
+  });
+  const getTime = (date) => {
+    let now = new Date();
+    let remainTime = (new Date(date) - now + 1000) / 1000;
+    let seconds = ("0" + Math.floor(remainTime % 60)).slice(-2);
+    let minutes = ("0" + Math.floor((remainTime / 60) % 60)).slice(-2);
+    let hours = ("0" + Math.floor((remainTime / 3600) % 24)).slice(-2);
+    let days = Math.floor(remainTime / (3600 * 24));
+    return {
+      remainTime,
+      seconds,
+      minutes,
+      hours,
+      days,
+    };
+  };
+  useEffect(() => {
+    const countDown = () => {
+      const timerUpdate = setInterval(() => {
+        setTime(getTime("Sun Nov 20 2022 13:00:00 GMT-0300"));
+      }, 1000);
+    };
+    countDown();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="info">
+        <img src={logo} alt="" />
+        <h1>Inicio de la copa del mundo</h1>
+        <div className="time">
+          <div>
+            <h3>{time.days && time.days}</h3>
+            <h4>Days</h4>
+          </div>
+          <div>
+            <h3>{time.hours && time.hours}</h3>
+            <h4>Hours</h4>
+          </div>
+          <div>
+            <h3>{time.minutes && time.minutes}</h3>
+            <h4>Minutes</h4>
+          </div>
+          <div>
+            <h3>{time.seconds && time.seconds}</h3>
+            <h4>Seconds</h4>
+          </div>
+        </div>
+      </div>
+      <div className="stadium">
+        {" "}
+        <img src={stadium} alt="" className="img_stadium" />
+        <img src={groups} alt="" className="groups" />
+      </div>
     </div>
   );
 }
